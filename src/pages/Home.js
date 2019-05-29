d8bf736f633696f7a637fe92de48de08d1ddd75c
user <user@lab1630m.tumo.lab>
2019-05-29 18:58:36 +0400

    Kayfot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,16 @@ export default function Home() {
   return (
     <div className="Home">
       <img
+      className="home-img img1"
         src="https://res.cloudinary.com/djwicpmah/image/upload/v1558684718/Red%20Square/60882626_436512253574644_1792717974960865280_n_irharv.jpg"
         alt="mec nkar"
-        width = "955"
-        height = "1220"
+       
       />
       <img
+      className = "home-img"
         src="https://res.cloudinary.com/djwicpmah/image/upload/v1558684718/Red%20Square/60856000_865848523831582_7582285361133387776_n_rondqq.jpg"
         alt="avatar"
-        width = "950"
-        height = "1220"
+       
         
       />
     </div>
